221a0ea9ea96dda09dc95fcbcd9c15b8690351d3
Narazaka <info@narazaka.net>
2018-04-06 13:27:36 +0900

    test

---
+++ test/match.js
@@ -0,0 +1,11 @@
+var assert = require("assert");
+
+var works = require("../all");
+
+var titles = Object.keys(works);
+
+for (var i = 0; i < titles.length; ++i) {
+    var title = titles[i];
+    var test = works[title];
+    assert(test.test(title), "[" + title + "] will match [" + test + "]");
+}
