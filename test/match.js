9d6f40916e7cff6c21e89f1bd880b0946158d4d3
Narazaka <info@narazaka.net>
2018-04-11 11:54:03 +0900

    test

--- test/match.js
+++ test/match.js
@@ -8,4 +8,5 @@ for (var i = 0; i < titles.length; ++i) {
     var title = titles[i];
     var test = works[title];
     assert(test.test(title), "[" + title + "] will match [" + test + "]");
+    assert(!test.test(""), "[" + title + "] will not match empty");
 }
